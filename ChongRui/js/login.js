f86b8306291f44ddee80a1eaccf8bfea8cdc6d65
gohc0078 <gohc0078@e.ntu.edu.sg>
2020-01-07 12:06:19 +0800

    added api

--- ChongRui/js/login.js
+++ ChongRui/js/login.js
@@ -2,15 +2,40 @@ const username = document.querySelector('input[name="user-name"]');
 const password = document.querySelector('input[name="password"]');
 const loginForm = document.querySelector('form');
 
+const user = username.value;
+
+
+
 loginForm.addEventListener('submit', (e) => {
+
+
     e.preventDefault();
+
+
     const user = username.value;
 
-    if (user === "MaryTan") {
-        console.log("Success");
+    const url = "http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/customers/" + encodeURIComponent(user);
 
-    }
 
+    fetch(url, {
+        method: "GET",
+        headers: {
+            "identity": "T43",
+            "token": "59368b55-3271-4dd8-821b-4f1a08fc36f5"
+
+        }
+    }).then(response => {
+        if (response.ok) {
+            response.json().then(data => {
+                console.log(data)
+            })
+        } else {
+            var error = new Error(reponse.statusText)
+            error.reponse = response
+            console.log("error1")
+        }
+
+    });
 
 
 })
